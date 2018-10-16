86f48c1c87bb8c1e1a311ef12993d295b13869cb
Achsah Abraham <achsahmp@gmail.com>
2018-10-16 22:13:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,17 +1,18 @@
 class Artist
- 
+  extend Memorable::ClassMethods
+  include Findable::InstanceMethods
   @@artists = []
- 
+
   attr_accessor :name
- 
+
   def initialize(name)
     @name = name
   end
- 
+
   def self.all
     @@artists
   end
- 
+
   def self.find_by_name(name)
     @@artists.detect {|a| a.name == name}
   end
